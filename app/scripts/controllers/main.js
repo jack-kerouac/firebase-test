793c8d080c9285306f8cd1fb5c98df64936988fd
Florian Rampp <florian.rampp@tado.com>
2015-02-19 11:56:26 +0100

    basic firebase key-value three-way bindings

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,5 +8,18 @@
  * Controller of the firebaseTestApp
  */
 angular.module('firebaseTestApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $firebase) {
+    var ref = new Firebase('https://radiant-fire-9056.firebaseio.com/');
+
+    // create an AngularFire reference to the data
+    var sync = $firebase(ref);
+
+    // download the data into a local object
+    var syncObject = sync.$asObject();
+    syncObject.$bindTo($scope, 'schedule');
+
+
+    $scope.temp = function (schedule, tempId) {
+      return _.findWhere(schedule.temperatures, {id: tempId}).degree;
+    };
   });
